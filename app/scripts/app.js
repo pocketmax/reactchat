cec6c67bd297071b6b98e676eddf04d1a51dee6a
pocketmax <sadsdf@adfsadf.com>
2015-02-21 03:04:07 -0700

    fixed #9

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,15 @@
 var React = window.React = require('react'),
 	ChatWindow = require('./ui/ChatWindow'),
 	ChatMessage = require('./ui/ChatMessage'),
-	Auth = require('./ui/Auth');
+	Auth = require('./ui/Auth'),
+	Router = require('react-router'),
+	DefaultRoute = Router.DefaultRoute,
+	Link = Router.Link,
+	Route = Router.Route,
+	RouteHandler = Router.RouteHandler;
 
 var ReactChat = React.createClass({
-	mixins: [ReactFireMixin],
+	mixins: [ReactFireMixin, Router.State],
 	getInitialState: function () {
 		return {
 			items: [],
@@ -13,7 +18,7 @@ var ReactChat = React.createClass({
 		};
 	},
 	componentWillMount: function () {
-		this.bindAsObject(new Firebase("https://burning-fire-3434.firebaseio.com/tagchat/chatrooms/2c5daa2710777330020edb18b0f1a5c4/messages"), "items");
+		this.bindAsObject(new Firebase("https://burning-fire-3434.firebaseio.com/tagchat/chatrooms/" + this.getParams().roomId + "/messages"), "items");
 	},
 	addMessage: function (msg) {
 		this.firebaseRefs.items.child(new Date().getTime().toString()).set(msg);
@@ -28,12 +33,19 @@ var ReactChat = React.createClass({
 
 	},
 	render: function () {
+		console.log(this.getParams());
 		return <div>
 			<ChatWindow items={this.state.items} />
 			<ChatMessage userProfile={this.state.userProfile} addAction={this.addMessage} />
 			<Auth userProfile={this.state.userProfile} doUserAuth={this.userAuth} />
+			<RouteHandler/>
 		</div>;
 	}
 });
-
-React.render(<ReactChat />, document.getElementById("app"));
+var routes = (
+	<Route name="app" path=":roomId" handler={ReactChat} />
+);
+Router.run(routes, function (Handler) {
+	React.render(<Handler />, document.getElementById("app"));
+//	React.render(<Handler />, document.body);
+});
