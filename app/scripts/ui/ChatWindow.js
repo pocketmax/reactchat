2acb431d88e283fbcbc2bef968bc5ec68b839c14
pocketmax <sadsdf@adfsadf.com>
2015-02-25 16:05:37 -0700

    issue #10
    * omg, now that looks better

--- app/scripts/ui/ChatWindow.js
+++ app/scripts/ui/ChatWindow.js
@@ -4,28 +4,27 @@ var React = require('react');
 var ChatWindow = React.createClass({
 	render: function () {
 		var items = this.props.items;
+		var listStyle = {
+			height: 310
+		};
+		var listGroupStyle = {
+			'text-align': 'right'
+		};
+
 		var createItem = function (val) {
-
-			var listStyle = {
-				height: 310
-			};
-			var listGroupStyle = {
-				'text-align': 'right'
-			};
-
-			return <div className="form-group">
-				<div scroll-glue className="list-group" style={listStyle}>
-					<div className="list-group-item list-group-item-success" style={listGroupStyle}>
+			return <div className="list-group-item list-group-item-success" style={listGroupStyle}>
 						<div class="row">
 							<b>{items[val]['from']}</b><br />
                             {items[val]['msg']}
 						</div>
-					</div>
-				</div>
 			</div>;
 		};
 		var keys = Object.keys(this.props.items);
-		return <div>{keys.map(createItem)}</div>;
+		return <div className="form-group">
+			<div scroll-glue className="list-group" style={listStyle}>
+				{keys.map(createItem)}
+			</div>
+		</div>;
 	}
 });
 
