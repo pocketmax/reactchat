165a78f6cfa448a8cdb40913a8c4777cae6a0ab3
pocketmax <sadsdf@adfsadf.com>
2015-02-25 15:08:40 -0700

    issue #10
    * Finally figured out that reactjs uses className and not normal class in jsx.
    * style is the same but req passing an object literal instead of a string.
    * For now, I'm just matching the bootstrap style from tagchat to save time.

--- app/scripts/ui/ChatWindow.js
+++ app/scripts/ui/ChatWindow.js
@@ -5,9 +5,23 @@ var ChatWindow = React.createClass({
 	render: function () {
 		var items = this.props.items;
 		var createItem = function (val) {
-			return <div>
-				<b>{items[val]['from']}</b>
-				<br/>{items[val]['msg']}
+
+			var listStyle = {
+				height: 310
+			};
+			var listGroupStyle = {
+				'text-align': 'right'
+			};
+
+			return <div className="form-group">
+				<div scroll-glue className="list-group" style={listStyle}>
+					<div className="list-group-item list-group-item-success" style={listGroupStyle}>
+						<div class="row">
+							<b>{items[val]['from']}</b><br />
+                            {items[val]['msg']}
+						</div>
+					</div>
+				</div>
 			</div>;
 		};
 		var keys = Object.keys(this.props.items);
